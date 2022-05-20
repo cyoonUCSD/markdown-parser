14202869ff2cd919ad90cbc15b58a5d38dd3a46d
cyoonUCSD <cayoon@ucsd.edu>
2022-05-20 16:48:07 -0700

    Update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,6 +5,13 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
+    public static void main(String[] args) throws IOException {
+		Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+        System.out.println(links);
+    }
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
@@ -30,11 +37,5 @@ public class MarkdownParse {
             }
         }
         return toReturn;
-    }
-    public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
-    }
+    }    
 }
