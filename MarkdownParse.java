6e0ec82ded4d4cd1ae41dd35a0239bd605544eba
cyoonUCSD <103143169+cyoonUCSD@users.noreply.github.com>
2022-05-22 17:07:22 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,41 +1,51 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-    public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
-    }
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
+        // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
+            //check to see if open bracket exists, break if not
+            if (markdown.indexOf("[", currentIndex) == -1) {
+                break;
+            }
+            
+            //check if ! and [ follow each other; skip 2 indices to avoid catching
+            //image link
+            if (markdown.indexOf("!", currentIndex) == markdown.indexOf("[", currentIndex)-1 && markdown.indexOf("!", currentIndex) != -1) {
+                currentIndex+=2;
+                continue;
+            }
+            
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+
+            //logic based specification: check if link is secured by checking
+            //for https://
+            if (markdown.indexOf("https://", openParen) == -1) {
+                currentIndex++;
+                continue;
+            }
             int closeParen = markdown.indexOf(")", openParen);
-            if(nextOpenBracket == -1 || nextCloseBracket == -1
-                  || closeParen == -1 || openParen == -1) {
-                return toReturn;
-            }
-            String potentialLink = markdown.substring(openParen + 1, closeParen);
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
-                toReturn.add(potentialLink);
-                currentIndex = closeParen + 1;
-            }
-            else {
-                currentIndex = currentIndex + 1;
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
         return toReturn;
-    }    
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
+    }
 }
